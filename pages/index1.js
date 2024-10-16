463636d650671c82a7e5d48775eeae7560a34a2a
liuketao <89596593@qq.com>
2024-10-16 10:22:15 +0800

    Update index1.js

--- pages/index1.js
+++ pages/index1.js
@@ -1,5 +1,5 @@
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session, status } = useSession();
